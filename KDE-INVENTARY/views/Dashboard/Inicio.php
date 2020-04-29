c3d241e6ecddbe1d0445b31c223ca36920d59e11
KevinAlvarez967 <53799682+KevinAlvarez967@users.noreply.github.com>
2020-04-29 13:32:20 -0600

    Template Dashboard

---
+++ KDE-INVENTARY/views/Dashboard/Inicio.php
@@ -0,0 +1,30 @@
+<?php
+require_once('../../core/helpers/TemplateDash.php');
+$pagina = new page;
+$pagina->headerTemplate('Inicio');
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+ $pagina->footerTemplate();       
+?>
